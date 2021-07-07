115adb950015cf572d0b382e653b088563724790
zaleck-mov <alexisgunitt@gmail.com>
2021-07-07 05:00:58 -0500

    add, delete and save storage

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pointer } from 'src/app/classes/pointer.class';
 
 @Component({
   selector: 'app-map',
@@ -7,12 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  pointers: Pointer[] = [];
+
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor() { }
+  constructor() { 
+    const pointersStorage = localStorage.getItem('pointer');
+    if(pointersStorage){
+      this.pointers = JSON.parse(pointersStorage);
+    }
+  }
 
   ngOnInit(): void {
+  }
+
+  addPointer(e: any){
+
+    const cords: { lat: number, lng: number } = e.coords
+    
+    const newPointer = new Pointer(cords.lat, cords.lng);
+    this.pointers.push(newPointer);
+    
+    localStorage.setItem('pointer', JSON.stringify(this.pointers));
+    this.saveStorage();
+  }
+
+  saveStorage(){
+    localStorage.setItem('pointer', JSON.stringify(this.pointers));
+  }
+
+  deletePointer(index: number){
+    this.pointers.splice(index, 1);
+    this.saveStorage();
   }
 
 }
